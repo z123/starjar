2ae98f58e24bc1d1d1295e77a9672176a5ab768f
z <z@zs-MacBook-Pro.local>
2015-11-11 22:55:44 -0800

    customizng styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,34 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-
+    
 var config = {
-    bootstrapDir: './node_modules/bootstrap-sass',
-    bootswatchDir: './node_modules/bootswatch',
-    publicDir: './public'
+  bootstrapDir: './node_modules/bootstrap-sass',
+  bootswatchDir: './node_modules/bootswatch',
+  publicDir: './public'
 };
 
 gulp.task('css', function() {
-    return gulp.src('./app/static/css/app.scss')
-    .pipe(sass({
-        includePaths: [config.bootstrapDir + '/assets/stylesheets',
-                       config.bootswatchDir],
-    }))
-    .pipe(gulp.dest(config.publicDir + '/css'));
+  return gulp.src('./app/static/css/app.scss')
+  .pipe(sass({
+    includePaths: [config.bootstrapDir + '/assets/stylesheets',
+                   config.bootswatchDir],
+  }))
+  .pipe(gulp.dest(config.publicDir + '/css'));
 });
 
 gulp.task('fonts', function() {
-    return gulp.src(config.bootstrapDir + '/assets/fonts/**/*')
-    .pipe(gulp.dest(config.publicDir + '/fonts'));
+  return gulp.src(config.bootstrapDir + '/assets/fonts/**/*')
+  .pipe(gulp.dest(config.publicDir + '/fonts'));
 });
 
 gulp.task('js', function() {
-    // TODO: Put bootstrap js in here to?
-    return gulp.src(['./app/static/js/app.js'])
+  // TODO: Put bootstrap js in here to?
+  return gulp.src(['./app/static/js/app.js'])
 });
 
 gulp.task('default', ['css', 'fonts', 'js']);
+
+gulp.watch('./app/static/css/**/*.scss', ['css'])
+  .on('change', function (event) {
+    console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');       
+});
